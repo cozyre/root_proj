9134accf64120261a3eaa396c20d8e13cc6f3641
Mickeytb <michael.timothy2006@gmail.com>
2026-06-30 21:24:02 +0700

    Test

--- Root/app/build.gradle.kts
+++ Root/app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
     implementation(libs.okhttp.logging)
+    implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.ui)
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.material3)
